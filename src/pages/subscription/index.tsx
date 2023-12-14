dd7909f5de878674e5cced3694744f61d5c5e92a
Hakushin <kpx1164@gmail.com>
2023-12-14 14:51:10 +0700

    added firebase

--- src/pages/subscription/index.tsx
+++ src/pages/subscription/index.tsx
@@ -1,12 +1,37 @@
 import { useDefaultLayout } from "@/hooks/useLayout";
-import type { NextPageWithLayout } from '@/utils/types';
+import type { NextPageWithLayout } from "@/utils/types";
+import { Playlist } from "@/components/lib/Playlist/index";
 const SubscriptionPage: NextPageWithLayout = () => {
   return (
     <>
-    <p className="Label">Sub Page</p>
+      <div className="Label">
+        <div className="HStack Lable gap-4">
+          <iframe
+            width="300px"
+            height="165px"
+            className="rounded-lg"
+            src="https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>{" "}
+          <div className="VStack justify-between">
+            <div className="">
+              <p className="Label font-medium text-lg">
+                영화 'Wonka'(2023) 예고편(trailer)으로 영어 공부하기
+              </p>
+              <p className="Grey">4 days ago</p>
+              <div className="HStack items-center gap-2 mt-4">
+                <img 
+                className="h-10 w-10"
+                src="https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png" alt="owner" />
+                <p>Yo Hippy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-   
+  );
 };
 SubscriptionPage.getLayout = useDefaultLayout;
 
--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,12 +1,142 @@
+"use client";
 import { useDefaultLayout } from "@/hooks/useLayout";
-import type { NextPageWithLayout } from '@/utils/types';
+import type { NextPageWithLayout } from "@/utils/types";
+import { storage } from "@/components/lib/Firebase/index";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+import {useRef, useState} from 'react';
+
 const UploadPage: NextPageWithLayout = () => {
+  const [dragActive, setDragActive] = useState<boolean>(false);
+  const inputRef = useRef<any>(null);
+  const [files, setFiles] = useState<any>([]);
+  function handleChange(e: any) {
+    e.preventDefault();
+    console.log("File has been added");
+    if (e.target.files && e.target.files[0]) {
+      // Allow only one file to be selected
+      setFiles([e.target.files[0]]);
+    }
+  }
+
+  async function handleSubmitFile(e: any) {
+    e.preventDefault();
+    if (files.length === 0) {
+      // no file has been submitted
+    } else {
+      // upload the file to Firebase Storage
+      const file = files[0];
+      const storageRef = ref(storage, `uploads/${file.name}`);
+      
+      try {
+        await uploadBytes(storageRef, file);
+        // Get the download URL for the uploaded file
+        const downloadURL = await getDownloadURL(storageRef);
+        console.log("File uploaded:", downloadURL);
+        // Handle further actions, e.g., saving the download URL to Firestore
+      } catch (error: any) {
+        console.error("Error uploading file:", error.message);
+        // Handle error
+      }
+    }
+  }
+
+  function handleDrop(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      for (let i = 0; i < e.dataTransfer.files["length"]; i++) {
+        setFiles((prevState: any) => [...prevState, e.dataTransfer.files[i]]);
+      }
+    }
+  }
+
+  function handleDragLeave(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+  }
+
+  function handleDragOver(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(true);
+  }
+
+  function handleDragEnter(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(true);
+  }
+
+  function removeFile(fileName: any, idx: any) {
+    const newArr = [...files];
+    newArr.splice(idx, 1);
+    setFiles([]);
+    setFiles(newArr);
+  }
+
+  function openFileExplorer() {
+    inputRef.current.value = "";
+    inputRef.current.click();
+  }
+
   return (
-    <>
-    <p className="Label">Upload Page</p>
-    </>
-  )
-   
+    <div className="flex items-center justify-center h-screen">
+      <form
+        className={`${
+          dragActive ? "bg-blue-400" : "bg-blue-100"
+        }  p-4 w-1/3 rounded-lg  min-h-[10rem] text-center flex flex-col items-center justify-center`}
+        onDragEnter={handleDragEnter}
+        onSubmit={handleSubmitFile} 
+        onDrop={handleDrop}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+      >
+
+
+       <input
+          placeholder="fileInput"
+          className="hidden"
+          ref={inputRef}
+          type="file"
+          multiple={true}
+          onChange={handleChange}
+          accept=".xlsx,.xls,image/*,.doc, .docx,.ppt, .pptx,.txt,.pdf"
+        />
+
+        <p>
+          Drag & Drop files or{" "}
+          <span
+            className="font-bold text-blue-600 cursor-pointer"
+            onClick={openFileExplorer}
+          >
+            <u>Select files</u>
+          </span>{" "}
+          to upload
+        </p>
+
+      <div className="flex flex-col items-center p-3">
+          {files.map((file: any, idx: any) => (
+            <div key={idx} className="flex flex-row space-x-5">
+              <span>{file.name}</span>
+              <span
+                className="text-red-500 cursor-pointer"
+                onClick={() => removeFile(file.name, idx)}
+              >
+                remove
+              </span>
+            </div>
+          ))}
+        </div>
+ 
+        <button type="submit" className="mt-4 bg-blue-500 text-white p-2 rounded-md">
+          Upload
+        </button>
+      </form>
+    </div>
+  );
 };
 UploadPage.getLayout = useDefaultLayout;
 
