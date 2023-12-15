223d1b18e1d13c705c8c69d127cea7ba600a409d
Hakushin <kpx1164@gmail.com>
2023-12-16 01:32:41 +0700

    UI2.0(Beta 1b) - Add animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,25 @@ import type { NextPageWithLayout } from "@/utils/types";
 const HomePage: NextPageWithLayout = () => {
   return (
     <>
-      <div className="p-14">
-        <p className="Title text-3xl">What is new in UI 2.0?</p>
-        <p className=" text-xl">
-          + New font! <span className="Title text-xl">Urban</span> and{" "}
-          <span className="Sub-title text-xl">Allrounder</span> <br />
-          + New layout!
-          <br />+ New colour <span className="Ocean-blue ">
-            Ocean-blue
-          </span> and{" "}
-          <span className="Blue p-2 System-background-ocean-blue rounded-lg">
-            Blue
-          </span>
-        </p>
+      <div className="h-screen bg-white">
+        {" "}
+        <div className="p-14">
+          <p className="Title text-3xl">What is new in UI 2.0?</p>
+          <p className=" text-xl">
+            + New font! <span className="Title text-xl">Urban</span> and{" "}
+            <span className="Sub-title text-xl">Allrounder</span> <br />
+            + New layout!
+            <br />+ New colour <span className="Ocean-blue ">
+              Ocean-blue
+            </span>{" "}
+            and{" "}
+            <span className="Blue p-2 System-background-ocean-blue rounded-lg">
+              Blue
+            </span>
+            <br />
+            + New animation! 2x smooth!
+          </p>
+        </div>
       </div>
     </>
   );
