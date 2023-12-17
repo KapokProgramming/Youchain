959d1c1b65f1628a27f4609b38a2e65127ee79f7
KAN KATPARK <kan_k@cmu.ac.th>
2023-12-17 22:17:00 +0700

    commit before merge

--- src/components/lib/Firebase/index.ts
+++ src/components/lib/Firebase/index.ts
@@ -3,7 +3,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 import firebaseConfig from "@/lib/db/firebaseConfig";
 
-console.log(firebaseConfig);
+// console.log(firebaseConfig);
 
 let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore();
