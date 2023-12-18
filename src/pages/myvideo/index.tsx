026e21e225b0b513f50c3257199628e09cd6207f
deityzs <kongkonghihi@gmail.com>
2023-12-18 13:34:32 +0700

    Update video components

--- src/pages/myvideo/index.tsx
+++ src/pages/myvideo/index.tsx
@@ -1,53 +1,59 @@
 import { useDefaultLayout } from "@/hooks/useLayout";
 import type { NextPageWithLayout } from "@/utils/types";
 import { Playlist } from "@/components/lib/Playlist/index";
+import Link from "next/link";
 const MyVideoPage: NextPageWithLayout = () => {
   const videoDetails = [
     {
+      id: "123",
       name: "영화 'Wonka'(2023) 예고편(trailer)으로 영어 공부하기",
       date: "4 days ago",
       channelName: "Yo Hippy",
-      thumbnail: "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+      thumbnail:
+        "https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
       url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
     },
     {
+      id: "124",
       name: "Wonka (2023)",
       date: "4 days ago",
       channelName: "Yo Hippy",
-      thumbnail: "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+      thumbnail:
+        "https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
       url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
     },
   ];
 
   function displayVideoDetails(detail: any) {
-    const details = []
+    const details = [];
     for (let i = 0; i < videoDetails.length; i++) {
-      details.push( 
-        <div className="HStack Lable gap-4 mb-4">
-          <iframe
-            width="300px"
-            height="165px"
-            className="rounded-lg "
-            src= { detail[i].url }
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          ></iframe>{" "}
-          <div className="VStack justify-between">
-            <div className="">
-              <p className="Label font-medium text-lg">
-                { detail[i].name }
-              </p>
-              <p className="Grey">4 days ago</p>
-              <div className="HStack items-center gap-2 mt-4">
+      details.push(
+        <div className="HStack Lable gap-2  hover:bg-gray-300 w-3/4 h-52">
+          <Link href={`play/${detail[i].id}`}>
+            <div className="HStack w-full">
+              <div className="video">
                 <img
-                  className="h-10 w-10 rounded-full"
-                  src= { detail[i].thumbnail }
-                  alt="owner"
+                  className="rounded-lg w-full h-48 object-fill"
+                  src={detail[i].thumbnail}
+                  alt="thumbnail"
                 />
-                <p> { detail[i].channelName } </p>
+              </div>
+              <div className="VStack justify-between">
+                <div className="">
+                  <p className="Label font-medium text-lg">{detail[i].name}</p>
+                  <p className="Grey">4 days ago</p>
+                  <div className="HStack items-center gap-2 mt-4">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src={detail[i].thumbnail}
+                      alt="owner"
+                    />
+                    <p> {detail[i].channelName} </p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
       );
     }
@@ -56,9 +62,7 @@ const MyVideoPage: NextPageWithLayout = () => {
 
   return (
     <>
-      <div className="Label">
-        { displayVideoDetails(videoDetails) }
-      </div>
+      <div className="Label ml-16 mt-10">{displayVideoDetails(videoDetails)}</div>
     </>
   );
 };
--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -3,12 +3,13 @@ import { useBosComponents } from "@/hooks/useBosComponents";
 import { useDefaultLayout } from "@/hooks/useLayout";
 import { useAuthStore } from "@/stores/auth";
 import type { NextPageWithLayout } from "@/utils/types";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const ProfilePage: NextPageWithLayout = () => {
   const components = useBosComponents();
-  const router = useRouter()
-  const profile_id = router.query.id
+  const router = useRouter();
+  const profile_id = router.query.id;
   // const accountId = useAuthStore((store) => store.accountId);
 
   const AccDetails = {
@@ -21,48 +22,54 @@ const ProfilePage: NextPageWithLayout = () => {
         name: "영화 'Wonka'(2023) 예고편(trailer)으로 영어 공부하기",
         date: "4 days ago",
         channelName: "Yo Hippy",
-        thumbnail:
+        logo:
           "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+        thumbnail:"https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
         url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
       },
       {
         name: "Wonka (2023)",
         date: "4 days ago",
         channelName: "Yo Hippy",
-        thumbnail:
+        logo:
           "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+        thumbnail:"https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
         url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
       },
       {
         name: "영화 'Wonka'(2023) 예고편(trailer)으로 영어 공부하기",
         date: "4 days ago",
         channelName: "Yo Hippy",
-        thumbnail:
+        logo:
           "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+        thumbnail:"https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
         url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
       },
       {
         name: "Wonka (2023)",
         date: "4 days ago",
         channelName: "Yo Hippy",
-        thumbnail:
+        logo:
           "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+        thumbnail:"https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
         url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
       },
       {
         name: "영화 'Wonka'(2023) 예고편(trailer)으로 영어 공부하기",
         date: "4 days ago",
         channelName: "Yo Hippy",
-        thumbnail:
+        logo:
           "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+        thumbnail:"https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
         url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
       },
       {
         name: "Wonka (2023)",
         date: "4 days ago",
         channelName: "Yo Hippy",
-        thumbnail:
+        logo:
           "https://i.pinimg.com/originals/56/f0/c7/56f0c7de57fdae6d0a9ddc43448b6dff.png",
+        thumbnail:"https://resizing.flixster.com/i7Txn-rTWv7B8aB5AYbe2IuLU6s=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE0N2E1MDljLTNlZDEtNDIyMS1iZjgzLTJkMjU1NjRjNzEwMy5qcGc=",
         url: "https://www.youtube.com/embed/wYmtRhKvmVE?si=wVWcqX8E2fXe_ZEk",
       },
     ],
@@ -71,16 +78,20 @@ const ProfilePage: NextPageWithLayout = () => {
     const details = [];
     for (let i = 0; i < AccDetails.videos.length; i++) {
       details.push(
-        <div className=" rounded-lg overflow-hidden w-full h-54 flex flex-col object-contain bg-gray-200 hover:bg-gray-500">
-          <iframe
-            src={AccDetails.videos[i].url}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          />
-          <p className="text-black w-full pl-2 whitespace-nowrap mt-2">
-            {AccDetails.videos[i].name}
-          </p>
-          <div className="pl-2"> {AccDetails.videos[i].date}</div>
-        </div>
+        <Link
+          href={ AccDetails.videos[i].url }
+        >
+          <div className=" rounded-lg overflow-hidden w-full h-54 flex flex-col object-contain bg-gray-200 hover:bg-gray-500">
+            <img
+              src={AccDetails.videos[i].thumbnail}
+              className="video object-fill w-full h-48"
+            />
+            <p className="text-black w-full pl-2 whitespace-nowrap mt-2">
+              {AccDetails.videos[i].name}
+            </p>
+            <div className="pl-2"> {AccDetails.videos[i].date}</div>
+          </div>
+        </Link>
       );
     }
     return details;
@@ -96,15 +107,16 @@ const ProfilePage: NextPageWithLayout = () => {
         />
       </div>
       <div>
-        <ComponentWrapperPage src={components.profileBanner} componentProps={ {accountId:profile_id} } />
+        <ComponentWrapperPage
+          src={components.profileBanner}
+          componentProps={{ accountId: profile_id }}
+        />
       </div>
       <div className="w-full h-full mt-2">
         <div className="flex flex-row w-full h-full mt-4">
-          <p className="text-black font-bold text-4xl ml-6 mt-6 ">
-            Videos
-          </p>
+          <p className="text-black font-bold text-4xl ml-6 mt-6 ">Videos</p>
           <div className=" w-5/6 h-5/6 text-lg">
-            <div className="grid grid-cols-4 h-full w-full mt-20 ml-32 gap-4">
+            <div className="grid grid-cols-4 h-full w-full mt-20 ml-22 gap-4">
               {displayVideoDetails(AccDetails)}
             </div>
           </div>
